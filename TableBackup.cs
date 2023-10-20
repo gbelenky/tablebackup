8df3fe64ee0a78c005a0c1e20dc349c3fe1e2298
gbelenky <26817885+gbelenky@users.noreply.github.com>
2023-10-20 17:42:05 +0200

    stable version

--- TableBackup.cs
+++ TableBackup.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.Logging;
 using Azure.Data.Tables;
 using Azure.Storage.Blobs;
 using System.Text.Json;
-using Azure;
 using System.Text;
 using Azure.Storage.Blobs.Specialized;
 
@@ -24,74 +23,49 @@ namespace GBelenky.TableBackup
 
 
         [Function(nameof(TableBackup))]
-        public static async Task<List<string>> RunOrchestrator(
-            [OrchestrationTrigger] TaskOrchestrationContext context)
+        public static async Task RunOrchestrator(
+             [OrchestrationTrigger] TaskOrchestrationContext context)
         {
             ILogger logger = context.CreateReplaySafeLogger(nameof(TableBackup));
-
             logger.LogInformation("Reading all table entities");
 
             var tableConnectionString = Environment.GetEnvironmentVariable("TableStorageConnection");
             var tableClient = new TableClient(new Uri(tableConnectionString));
+            var backupBlob = Environment.GetEnvironmentVariable("BackupBlob");
+            
+            DateTime now = context.CurrentUtcDateTime;
+            string filePrefix = $"{now.Year}-{now.Month}-{now.Day}-{now.Hour}-{now.Minute}-{now.Second}-";
+            backupBlob = filePrefix + backupBlob;
 
             int pageSize = 1000;
-            string continuationToken = null;
+            string? continuationToken = null;
             do
             {
                 var page = tableClient.Query<TableEntity>().AsPages(continuationToken, pageSize).First();
-                // convert page.Values to json
                 var json = JsonSerializer.Serialize(page.Values);
-                await context.CallActivityAsync(nameof(BackupPage), json);
+                ActivityParams activityParams = new ActivityParams { Json = json, BackupBlob = backupBlob };
+                await context.CallActivityAsync(nameof(BackupPage), activityParams);
                 continuationToken = page.ContinuationToken;
             } while (continuationToken != null);
-
-
-            var outputs = new List<string>();
-
-            return outputs;
-        }
-
-       [Function(nameof(BackupPage))]
-        public async Task BackupPage([ActivityTrigger] string pageValues, FunctionContext executionContext)
-        {
-            ILogger logger = executionContext.GetLogger("BackupPage");
-            logger.LogInformation("Writing page to blob storage");
-            var backupBlob = Environment.GetEnvironmentVariable("BackupBlob");
-            // Get a reference to the container and the append blob
-            var appendBlobClient = _blobContainerClient.GetAppendBlobClient(backupBlob);
-
-            // Create the append blob if it doesn't exist
-            await appendBlobClient.CreateIfNotExistsAsync();
-
-            // Convert the JSON string to bytes
-            byte[] bytes = Encoding.UTF8.GetBytes(pageValues);
-
-            // Write the bytes to the append blob
-            using (var memoryStream = new MemoryStream(bytes))
-            {
-                await appendBlobClient.AppendBlockAsync(memoryStream);
-            }
 
             return;
         }
 
-/*
         [Function(nameof(BackupPage))]
-        public async Task BackupPage([ActivityTrigger] string pageValues, FunctionContext executionContext)
+        public async Task BackupPage([ActivityTrigger] ActivityParams aParams, FunctionContext executionContext)
         {
             ILogger logger = executionContext.GetLogger("BackupPage");
             logger.LogInformation("Writing page to blob storage");
-            var backupBlobContainer = Environment.GetEnvironmentVariable("BackupBlobContainer");
-            var backupBlob = Environment.GetEnvironmentVariable("BackupBlob");
+            
+
             // Get a reference to the container and the append blob
-            var containerClient = _blobClient.GetBlobContainerClient(backupBlobContainer);
-            var appendBlobClient = containerClient.GetAppendBlobClient(backupBlob);
+            var appendBlobClient = _blobContainerClient.GetAppendBlobClient(aParams.BackupBlob);
 
             // Create the append blob if it doesn't exist
             await appendBlobClient.CreateIfNotExistsAsync();
 
             // Convert the JSON string to bytes
-            byte[] bytes = Encoding.UTF8.GetBytes(pageValues);
+            byte[] bytes = Encoding.UTF8.GetBytes(aParams.Json);
 
             // Write the bytes to the append blob
             using (var memoryStream = new MemoryStream(bytes))
@@ -99,54 +73,6 @@ namespace GBelenky.TableBackup
                 await appendBlobClient.AppendBlockAsync(memoryStream);
             }
 
-            return;
-        }
-*/
-
-        // read all tables uris from storage account        
-        [Function(nameof(ReadTable))]
-        public static async Task ReadTable([ActivityTrigger] FunctionContext executionContext)
-        {
-            ILogger logger = executionContext.GetLogger("ReadTable");
-            logger.LogInformation("Reading all table entities");
-
-            var tableConnectionString = Environment.GetEnvironmentVariable("TableStorageConnection");
-            var tableClient = new TableClient(new Uri(tableConnectionString));
-
-            int pageSize = 1000;
-            string continuationToken = null;
-            do
-            {
-                var page = tableClient.Query<TableEntity>().AsPages(continuationToken, pageSize).First();
-                int i = 0;
-                foreach (TableEntity qEntity in page.Values)
-                {
-                    logger.LogInformation($"Number in page:{i} RowKey: {qEntity.GetString("RowKey")}");
-                    i++;
-                }
-                // Call your method here after processing each page
-                logger.LogInformation($"******************Entities returned: {page.Values.Count()}******************");
-                continuationToken = page.ContinuationToken;
-            } while (continuationToken != null);
-            /*           
-                        Pageable<TableEntity> queryResultsFilter = tableClient.Query<TableEntity>();
-
-                        foreach (TableEntity qEntity in queryResultsFilter)
-                        {
-                            logger.LogInformation($"RowKey: {qEntity.GetString("RowKey")}");
-                        }
-
-                         logger.LogInformation($"The query returned {queryResultsFilter.Count()} entities.");
-            */
-            // save entities to blob storage
-            /*
-            var blobConnectionString = Environment.GetEnvironmentVariable("BlobStorageConnection");
-            var blobClient = new BlobClient(new Uri(blobConnectionString));
-            var json = JsonSerializer.Serialize(entities);
-            var bytes = Encoding.UTF8.GetBytes(json);
-            var stream = new MemoryStream(bytes);
-            await blobClient.UploadAsync(stream);
-            */
             return;
         }
 
@@ -168,5 +94,11 @@ namespace GBelenky.TableBackup
             // See https://learn.microsoft.com/azure/azure-functions/durable/durable-functions-http-api#start-orchestration
             return client.CreateCheckStatusResponse(req, instanceId);
         }
+    }
+
+    public class ActivityParams
+    {
+        public string Json { get; set; }
+        public string BackupBlob { get; set; }
     }
 }
