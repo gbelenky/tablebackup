0d76d6d1c8a8bcb0ff8833088e75c9fbaf3da420
gbelenky <26817885+gbelenky@users.noreply.github.com>
2023-10-26 12:14:09 +0200

    adding a Single Activity

--- TableBackup.cs
+++ TableBackup.cs
@@ -24,9 +24,9 @@ namespace GBelenky.TableBackup
             _tableClient = tableClient;
         }
 
- [Function("TableBackupByTimestamp")]
+        [Function("TableBackupByTimestamp")]
         public async Task RunOrchestratorByTimestamp(
-             [OrchestrationTrigger] TaskOrchestrationContext context)
+                    [OrchestrationTrigger] TaskOrchestrationContext context)
         {
             ILogger logger = context.CreateReplaySafeLogger("TableBackupByTimestamp");
             string backupBlobSetting = Environment.GetEnvironmentVariable("BackupBlob") ?? "backup.json";
@@ -35,7 +35,7 @@ namespace GBelenky.TableBackup
             DateTime now = context.CurrentUtcDateTime;
             string filePrefix = context.InstanceId + "-ByTimestamp-";
             backupBlob = filePrefix + backupBlobSetting;
-            
+
             pageSizeSetting = Int32.Parse(Environment.GetEnvironmentVariable("PageSize") ?? "1000");
 
             // min valid date for Azure Table Storage
@@ -60,7 +60,7 @@ namespace GBelenky.TableBackup
                 }
             }
             DateTime end = context.CurrentUtcDateTime;
-            logger.LogInformation($"Backup completed with Total Rows: {rowCount} Pages: {pages} Last row date: {lastRowDate} Total Time seconds : {(end - now).TotalSeconds}");;
+            logger.LogInformation($"Backup completed with Total Rows: {rowCount} Pages: {pages} Last row date: {lastRowDate} Total Time seconds : {(end - now).TotalSeconds}"); ;
             return;
         }
 
@@ -103,6 +103,70 @@ namespace GBelenky.TableBackup
             }
             DateTime end = context.CurrentUtcDateTime;
             logger.LogInformation($"Backup completed with Total Rows: {rows} Total Bytes: {backupBytes} Total Time seconds : {(end - now).TotalSeconds}");
+        }
+
+        [Function("TableBackupSingleActivity")]
+        public async Task RunOrchestratorSingleActivity(
+                     [OrchestrationTrigger] TaskOrchestrationContext context)
+        {
+            ILogger logger = context.CreateReplaySafeLogger("TableBackupSingleActivity");
+            string backupBlobSetting = Environment.GetEnvironmentVariable("BackupBlob") ?? "backup.json";
+            int pageSizeSetting = Int32.Parse(Environment.GetEnvironmentVariable("PageSize") ?? "1000");
+
+            DateTime now = context.CurrentUtcDateTime;
+            string filePrefix = context.InstanceId + "-SingleActivity-";
+            backupBlob = filePrefix + backupBlobSetting;
+
+            logger.LogInformation($"Starting backup at: {now}}}");
+
+            int rowCount = await context.CallActivityAsync<int>(nameof(BackupAllSingleActivity), backupBlob);
+
+            DateTime end = context.CurrentUtcDateTime;
+            logger.LogInformation($"Backup completed with Total Rows: {rowCount} Total Time seconds : {(end - now).TotalSeconds}"); ;
+            return;
+        }
+
+
+        [Function(nameof(BackupAllSingleActivity))]
+        public async Task<int> BackupAllSingleActivity([ActivityTrigger] string backupBlob, FunctionContext executionContext)
+        {
+            string continuationToken = null;
+            bool moreResultsAvailable = true;
+            int pageSizeSetting = Int32.Parse(Environment.GetEnvironmentVariable("PageSize") ?? "1000");
+            
+            int rows = 0;
+            while (moreResultsAvailable)
+            {
+                Azure.Page<TableEntity> page = _tableClient
+                    .Query<TableEntity>()
+                    .AsPages(continuationToken, pageSizeHint: pageSizeSetting)
+                    .FirstOrDefault(); // Note: Since the pageSizeHint only limits the number of results in a single page, we explicitly only enumerate the first page.
+
+                if (page == null)
+                    break;
+
+                // Get the continuation token from the page.
+                // Note: This value can be stored so that the next page query can be executed later.
+                continuationToken = page.ContinuationToken;
+
+                IReadOnlyList<TableEntity> pageResults = page.Values;
+                moreResultsAvailable = pageResults.Any() && continuationToken != null;
+                rows += pageResults.Count;
+                TableEntity[] valuesArray = pageResults.ToArray();
+                
+                var appendBlobClient = _blobContainerClient.GetAppendBlobClient(backupBlob);
+                // Create the append blob if it doesn't exist
+                await appendBlobClient.CreateIfNotExistsAsync();
+                var json = JsonSerializer.Serialize(valuesArray);
+                byte[] bytes = Encoding.UTF8.GetBytes(json);
+                // Write the bytes to the append blob
+                using (var memoryStream = new MemoryStream(bytes))
+                {
+                    await appendBlobClient.AppendBlockAsync(memoryStream);
+                }
+         
+            }
+            return rows;
         }
 
         [Function(nameof(BackupPageByQuery))]
@@ -203,6 +267,30 @@ namespace GBelenky.TableBackup
             };
             string instanceId = await client.ScheduleNewOrchestrationInstanceAsync(
                 "TableBackupPageByQuery", options: options, input: null);
+
+            logger.LogInformation("Started orchestration with ID = '{instanceId}'.", instanceId);
+
+            // Returns an HTTP 202 response with an instance management payload.
+            // See https://learn.microsoft.com/azure/azure-functions/durable/durable-functions-http-api#start-orchestration
+            return client.CreateCheckStatusResponse(req, instanceId);
+        }
+
+        [Function("TableBackup_HttpStart_SingleActivity")]
+        public static async Task<HttpResponseData> HttpStartSingleActivity(
+          [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req,
+          [DurableClient] DurableTaskClient client,
+          FunctionContext executionContext)
+        {
+            ILogger logger = executionContext.GetLogger("TableBackup_HttpStart_SingleActivity");
+
+            // Function input comes from the request content.
+            DateTime now = DateTime.UtcNow;
+            StartOrchestrationOptions? options = new StartOrchestrationOptions()
+            {
+                InstanceId = $"{now.Year}-{now.Month}-{now.Day}-{now.Hour}-{now.Minute}-{now.Second}"
+            };
+            string instanceId = await client.ScheduleNewOrchestrationInstanceAsync(
+                "TableBackupSingleActivity", options: options, input: null);
 
             logger.LogInformation("Started orchestration with ID = '{instanceId}'.", instanceId);
 
